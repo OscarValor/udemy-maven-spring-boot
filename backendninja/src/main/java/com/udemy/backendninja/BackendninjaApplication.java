b21d3d3cca17d8568a519b65aeb7b9ae767703c2
ovalor <ovalor@gmail.com>
2019-12-20 11:42:27 +0000

    subida estructrua proyecto maven-spring

---
+++ backendninja/src/main/java/com/udemy/backendninja/BackendninjaApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.backendninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendninjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendninjaApplication.class, args);
+	}
+
+}
